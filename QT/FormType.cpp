2bcf1e7a8176eb6a88c4c14d3d4e3a56e4ea58b7
farhanrashid <farhan.rashid@live.com>
2022-12-29 19:17:22 +0100

    renamed

---
+++ QT/FormType.cpp
@@ -0,0 +1,35 @@
+#include "FormType.h"
+
+void FT_DialogWithButtonsBottom::Generate(QString ProjDir, QString SubDir, QString FormName)
+{
+    ProjDir = "";
+    SubDir  = "";
+}
+
+void FT_DialogWithButtonsRight::Generate(QString ProjDir, QString SubDir, QString FormName)
+{
+    ProjDir = "";
+    SubDir  = "";
+
+}
+
+void FT_DialogWithButtons::Generate(QString ProjDir, QString SubDir, QString FormName)
+{
+    ProjDir = "";
+    SubDir  = "";
+
+}
+
+void FT_MainWindow::Generate(QString ProjDir, QString SubDir, QString FormName)
+{
+    ProjDir = "";
+    SubDir  = "";
+
+}
+
+void FT_Widget::Generate(QString ProjDir, QString SubDir, QString FormName)
+{
+    ProjDir = "";
+    SubDir  = "";
+}
+
---
+++ QT/FormType.h
@@ -0,0 +1,51 @@
+#ifndef FORMTYPE_H
+#define FORMTYPE_H
+
+#include <QString>
+#define Dialog_With_Buttons_Bottom "Dialog with Buttons Bottom"
+#define Dialog_With_Buttons_Right "Dialog with Buttons Right"
+#define Dialog_With_Buttons "Dialog with Buttons"
+#define Main_Window "Main Window"
+#define Widget "Widget"
+
+class FormType
+{
+public:
+    virtual void Generate(QString ProjDir, QString SubDir, QString FormName) = 0;
+    virtual ~FormType(){}
+
+};
+
+class FT_DialogWithButtonsBottom : public FormType
+{
+public:
+    void Generate(QString ProjDir, QString SubDir, QString FormName);
+};
+
+
+class FT_DialogWithButtonsRight : public FormType
+{
+public:
+    void Generate(QString ProjDir, QString SubDir, QString FormName);
+};
+
+class FT_DialogWithButtons : public FormType
+{
+public:
+    void Generate(QString ProjDir, QString SubDir, QString FormName);
+};
+
+class FT_MainWindow : public FormType
+{
+public:
+    void Generate(QString ProjDir, QString SubDir, QString FormName);
+};
+
+class FT_Widget : public FormType
+{
+public:
+    void Generate(QString ProjDir, QString SubDir, QString FormName);
+};
+
+
+#endif // FORMTYPE_H
